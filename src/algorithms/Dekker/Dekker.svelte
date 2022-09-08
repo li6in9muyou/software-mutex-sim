252ec8b517da3d095fba657bc8292bf809b49c64
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-09-08 14:33:05 +0800

    feat: dekker's uses web worker

--- src/algorithms/Dekker/Dekker.svelte
+++ src/algorithms/Dekker/Dekker.svelte
@@ -1,18 +1,76 @@
 <script>
-  import {
-    process_status,
-    those_in_critical_region,
-    those_contending,
-  } from "../../SveltePort";
-  import { wants_to_enter_store, turn_store } from "./DekkerPort";
-  import { process_zero, process_one } from "./Dekker";
+  import { SveltePort } from "../../SveltePort";
   import { onMount } from "svelte";
+  import { Yield } from "../../utility";
+  import { writable } from "svelte/store";
 
-  console.info("Dekker's begins");
+  const wants_to_enter_store = writable([false, false]);
+  const turn_store = writable([0]);
+  const port = new SveltePort(2);
+  const those_in_critical_region = port.those_in_critical_region;
+  const those_contending = port.those_contending;
+  const process_status = port.process_status;
+
+  const workers = [];
+  let killed_process_count = 0;
+  function handleWorkerMessage(worker, ev) {
+    const d = ev.data;
+    switch (d.type) {
+      case "pre": {
+        port.pre_critical_region(d.who);
+        break;
+      }
+      case "post": {
+        port.post_critical_region(d.who);
+        break;
+      }
+      case "done": {
+        workers[d.who].terminate();
+        console.log(`${d.who} is killed`);
+        killed_process_count += 1;
+        break;
+      }
+      case "sync_store": {
+        const { wants_to_enter, turn } = d;
+        if (wants_to_enter !== undefined) {
+          wants_to_enter_store.set(
+            Array.from(wants_to_enter).map((i) => i === 99)
+          );
+        }
+        if (turn !== undefined) {
+          turn_store.set(turn);
+        }
+        break;
+      }
+      default: {
+        console.info(d);
+      }
+    }
+  }
+
   onMount(async () => {
     while (true) {
-      await process_one();
-      await process_zero();
+      console.info("Dekker's begins");
+      const wte = new SharedArrayBuffer(2);
+      const t = new SharedArrayBuffer(1);
+      killed_process_count = 0;
+      for (let i = 0; i < 2; i++) {
+        const w = new Worker(new URL("./Dekker.ts", import.meta.url), {
+          type: "module",
+          name: `dekker's no.${i}` /* @vite-ignore */,
+        });
+        w.onmessage = (d) => handleWorkerMessage(w, d);
+        w.onerror = (ev) => console.error(`${i} error: ` + ev.data);
+        workers[i] = w;
+        w.postMessage({
+          me: i,
+          context: { turn: t, wants_to_enter: wte },
+        });
+      }
+      do {
+        await Yield();
+      } while (killed_process_count < 2);
+      console.info("Dekker's finishes");
     }
   });
 </script>
--- src/algorithms/Dekker/Dekker.ts
+++ src/algorithms/Dekker/Dekker.ts
@@ -1,28 +1,60 @@
-import { critical_region, Yield } from "../../utility";
-import portBuilder from "./DekkerPort";
+import { Idle, Yield } from "../../utility";
 
-export function counterpart(id) {
+interface RawCtx {
+  wants_to_enter: ArrayBuffer;
+  turn: ArrayBuffer;
+}
+
+const TRUE = 99;
+const FALSE = -99;
+
+interface CookedCtx {
+  wants_to_enter: Int8Array;
+  turn: number;
+}
+
+function counterpart(id) {
   return id === 1 ? 0 : 1;
 }
 
-async function process_x(id) {
-  const port = portBuilder(id);
+function extract(context: RawCtx): CookedCtx {
+  return {
+    wants_to_enter: new Int8Array(context.wants_to_enter),
+    turn: new Int8Array(context.turn)[0],
+  };
+}
 
-  port.set_my_interests();
-  while (port.if_counterpart_interests()) {
-    if (!port.if_my_turn()) {
-      port.clear_my_interests();
-      while (!port.if_my_turn()) {
+async function lock(context: RawCtx, id) {
+  const { wants_to_enter, turn } = extract(context);
+  wants_to_enter[id] = TRUE;
+  self.postMessage({ type: "sync_store", wants_to_enter });
+  while (TRUE === wants_to_enter[counterpart(id)]) {
+    if (!turn === id) {
+      wants_to_enter[id] = FALSE;
+      self.postMessage({ type: "sync_store", wants_to_enter });
+      while (turn !== id) {
         await Yield();
         // busy waiting
       }
-      port.set_my_interests();
+      wants_to_enter[id] = TRUE;
+      self.postMessage({ type: "sync_store", wants_to_enter });
     }
   }
-  await critical_region(id)(id);
-  port.flip_turn();
-  port.clear_my_interests();
 }
 
-export const process_zero = () => process_x(0);
-export const process_one = () => process_x(1);
+function unlock(context: RawCtx, id) {
+  let { wants_to_enter, turn } = extract(context);
+  turn = counterpart(id);
+  wants_to_enter[id] = FALSE;
+  self.postMessage({ type: "sync_store", turn, wants_to_enter });
+}
+
+self.onmessage = async (ev) => {
+  const { me, context } = ev.data;
+  await lock(context, me);
+  self.postMessage({ type: "pre", who: ev.data.me });
+  await Idle();
+  self.postMessage({ type: "post", who: ev.data.me });
+  unlock(context, me);
+  self.postMessage({ type: "done", who: ev.data.me });
+};
