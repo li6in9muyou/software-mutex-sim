bcfb6b22b70de10333ae1f9e8a1640f82269af08
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-09-09 15:12:00 +0800

    refactor: EisenbergAndMcGuire.ts uses Labour class

--- src/algorithms/EisenbergAndMcGuire/EisenbergAndMcGuire.ts
+++ src/algorithms/EisenbergAndMcGuire/EisenbergAndMcGuire.ts
@@ -1,76 +1,79 @@
 import { Idle } from "../../utility";
+import Labour from "../../Labour";
+import type { IContext } from "../../Labour";
 
 const IDLE = 13;
 const IN_CS = 99;
 const WANT_IN = 42;
 
-interface Context {
+interface IEisenbergAndMcGuireMemory extends IContext {
   flag: Int8Array;
-  turn: number;
-  process_count: number;
+  turn: Int8Array;
 }
 
-function extract(context): Context {
-  return {
-    flag: new Int8Array(context.flag),
-    turn: new Int8Array(context.turn)[0],
-    process_count: context.process_count,
-  };
-}
+class EisenbergAndMcGuire extends Labour {
+  private readonly process_count: number;
 
-async function lock(context, i) {
-  let { process_count, flag, turn } = extract(context);
-  let j;
-  do {
-    flag[i] = WANT_IN;
-    j = turn;
-    self.postMessage({ type: "sync_store", flag });
+  constructor(who: number, process_count: number, context: IContext) {
+    super(who, context, Idle);
+    this.process_count = process_count;
+  }
 
-    while (j != i) {
-      if (flag[j] != IDLE) {
-        j = turn;
-      } else {
+  protected lock_impl(context: IEisenbergAndMcGuireMemory) {
+    const i = this.who;
+    const process_count = this.process_count;
+    let { flag, turn } = context;
+
+    let j;
+    do {
+      flag[i] = WANT_IN;
+      j = turn[0];
+
+      while (j != i) {
+        if (flag[j] != IDLE) {
+          j = turn[0];
+        } else {
+          j += 1;
+          j %= process_count;
+        }
+      }
+
+      flag[i] = IN_CS;
+
+      j = 0;
+      while (j < process_count && (j === i || flag[j] !== IN_CS)) {
         j += 1;
-        j %= process_count;
+      }
+      if (j >= process_count && (turn[0] == i || flag[turn[0]] == IDLE)) {
+        break;
+      }
+    } while (true);
+    turn[0] = i;
+  }
+
+  protected prepare_context_impl(context): IEisenbergAndMcGuireMemory {
+    return {
+      flag: new Int8Array(context.flag),
+      turn: new Int8Array(context.turn),
+    };
+  }
+
+  protected unlock_impl(context) {
+    let { flag, turn, process_count } = context;
+    let pid = (turn[0] + 1) % process_count;
+    while (true) {
+      pid += 1;
+      pid %= process_count;
+      if (flag[pid] !== IDLE) {
+        break;
       }
     }
-
-    flag[i] = IN_CS;
-    self.postMessage({ type: "sync_store", flag });
-
-    j = 0;
-    while (j < process_count && (j === i || flag[j] !== IN_CS)) {
-      j += 1;
-    }
-    if (j >= process_count && (turn == i || flag[turn] == IDLE)) {
-      break;
-    }
-  } while (true);
-  turn = i;
-  self.postMessage({ type: "sync_store", flag, turn });
-}
-
-async function unlock(context, i) {
-  let { flag, turn, process_count } = extract(context);
-  let pid = (turn + 1) % process_count;
-  while (true) {
-    pid += 1;
-    pid %= process_count;
-    if (flag[pid] !== IDLE) {
-      break;
-    }
+    turn[0] = pid;
+    flag[this.who] = IDLE;
   }
-  turn = pid;
-  flag[i] = IDLE;
-  self.postMessage({ type: "sync_store", turn, flag });
 }
 
 self.onmessage = async (ev) => {
   const { me, context } = ev.data;
-  await lock(context, me);
-  self.postMessage({ type: "pre", who: ev.data.me });
-  await Idle();
-  self.postMessage({ type: "post", who: ev.data.me });
-  await unlock(context, me);
-  self.postMessage({ type: "done", who: ev.data.me });
+  await new EisenbergAndMcGuire(me, context.process_count, context).run();
 };
