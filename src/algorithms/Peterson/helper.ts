d72e2613be71b35999e3c5d17b184250c51d95d4
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-09-20 09:11:12 +0800

    refactor: remove dead code

--- src/algorithms/Peterson/helper.ts
+++
@@ -1,5 +0,0 @@
-import { expose } from "threads";
-import { PetersonSimpleBuilder } from "./Peterson";
-import { build_thread_pool_task } from "../../Labour";
-
-expose(build_thread_pool_task(new PetersonSimpleBuilder()));
