042d73d7a550ee562e0dcbcc3dec2948cd00216d
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-05 15:00:35 +0800

    refactor: assign names to boolean flags

--- src/view/InSimulation/ProcessAgent.svelte
+++ src/view/InSimulation/ProcessAgent.svelte
@@ -2,11 +2,11 @@
   import { ProcessState } from "../../use_case/RunningSync";
   import type { ISingleProcessHandle } from "../../use_case/ProcessHandle";
 
-  export let pid: number;
-  export let selectedPid: number;
-  export let in_region: ProcessState;
-  export let procState: ProcessState;
-  export let ProcessHandle: ISingleProcessHandle;
+  export let pid: number = null;
+  export let selectedPid: number = null;
+  export let in_region: ProcessState = null;
+  export let procState: ProcessState = null;
+  export let ProcessHandle: ISingleProcessHandle = null;
 
   let showPauseSpinner = false;
   function handleToggle() {
@@ -28,13 +28,17 @@
     [ProcessState.running, "RUNNING"],
     [ProcessState.paused, "PAUSED"],
   ]);
+
+  $: isSelected = pid === selectedPid;
+  $: isIdle =
+    procState === ProcessState.ready || procState === ProcessState.completed;
 </script>
 
 <div
-  class:bg-secondary={pid === selectedPid}
-  class:text-accent-content={pid === selectedPid}
-  class:bg-base-100={pid !== selectedPid}
-  class:text-base-content={pid !== selectedPid}
+  class:bg-secondary={isSelected}
+  class:text-accent-content={isSelected}
+  class:bg-base-100={!isSelected}
+  class:text-base-content={!isSelected}
   class="flex items-center rounded border border-accent p-4 transition-colors duration-300"
   on:click={() => (selectedPid = pid)}
 >
@@ -42,13 +46,8 @@
     {`${procStateDisplay.get(procState)} pid ${pid} ` +
       `is ${in_region ? "in" : "not in "} critical region`}
   </div>
-  {#if procState === ProcessState.ready || procState === ProcessState.completed}
-    <button
-      class:btn-success={procState === ProcessState.ready ||
-        procState === ProcessState.completed}
-      class="btn btn-sm ml-auto"
-      on:click={ProcessHandle.run}
-    >
+  {#if isIdle}
+    <button class="btn btn-success btn-sm ml-auto" on:click={ProcessHandle.run}>
       {#if showPauseSpinner}
         <svg
           class="-ml-1 mr-3 h-5 w-5 animate-spin text-error"
@@ -75,7 +74,7 @@
     </button>
   {:else}
     <button
-      class:btn-disabled={pid !== selectedPid}
+      class:btn-disabled={!isSelected}
       class:btn-warning={procState === ProcessState.running}
       class:btn-success={procState === ProcessState.paused}
       class="btn btn-sm ml-auto"
