494be6092e9e1bf66d63184bc3005f4055580cd3
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-05 17:20:18 +0800

    feat: implemented AlgorithmConfig.svelte

--- src/algorithms/IStaticAlgorithmDescription.ts
+++ src/algorithms/IStaticAlgorithmDescription.ts
@@ -1,6 +1,7 @@
 interface IStaticAlgorithmDescription {
   name: string;
   description?: string;
+  max_process_count?: number;
   memory_transform: (raw: number) => string;
 }
 export default IStaticAlgorithmDescription;
--- src/view/AlgorithmConfig.svelte
+++ src/view/AlgorithmConfig.svelte
@@ -1,5 +1,10 @@
 <script>
   import { router } from "./model";
+  import { CurrentSelectedAlgorithm, ProcessCount } from "./algorithm_config";
+  import { range } from "lodash";
+
+  $: maxCount = Math.min($CurrentSelectedAlgorithm?.max_process_count ?? 13);
+  $: step = Math.floor((maxCount - 1) / 4);
 </script>
 
 <div class="navbar mb-2 rounded bg-base-200 shadow-xl">
@@ -12,4 +17,20 @@
     >
   </div>
 </div>
-<h1 class="font-4xl">config works</h1>
+
+<h1 class="text-2xl underline">How many processes to spawn?</h1>
+<h2 class="mt-16 mb-8 text-6xl">{$ProcessCount}</h2>
+<input
+  type="range"
+  min="1"
+  max={maxCount}
+  bind:value={$ProcessCount}
+  class="range range-primary range-lg"
+  step="1"
+/>
+<div class="flex w-full justify-between px-2">
+  {#each range(1, maxCount, step) as stop}
+    <span class="text-2xl">{stop}</span>
+  {/each}
+  <span class="text-2xl">{maxCount}</span>
+</div>
