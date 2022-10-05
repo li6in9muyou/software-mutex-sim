3729136a2bca641f80c1b5b8f5cb94f834be5dc9
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-05 14:38:19 +0800

    fix: encapsulate module variable with closures

--- src/use_case/BaseProcess.ts
+++ src/use_case/BaseProcess.ts
@@ -3,124 +3,128 @@ import { Observable, Subject } from "threads/observable";
 import { merge } from "observable-fns";
 import type { RunningSyncEvent } from "./RunningSync";
 
-let shouldPause = false;
-let _pauseAtEveryBreakpoint = false;
-let _resolve: (value: null) => void;
-let _pause: Promise<null>;
-let _i = 0;
-const _mem_msg = new Subject();
-const _core_msg = new Subject();
-const _running_sync_msg = new Subject<RunningSyncEvent>();
-const _debug_msg = new Subject();
-const dbg = (args) => _debug_msg.next(args);
+export default () => {
+  let shouldPause = false;
+  let _pauseAtEveryBreakpoint = false;
+  let _resolve: (value: null) => void;
+  let _pause: Promise<null>;
+  let _i = 0;
+  const _mem_msg = new Subject();
+  const _core_msg = new Subject();
+  const _running_sync_msg = new Subject<RunningSyncEvent>();
+  const _debug_msg = new Subject();
+  const dbg = (args) => _debug_msg.next(args);
 
-function pause() {
-  shouldPause = true;
-  _pause = new Promise((resolve) => {
-    _resolve = () => {
-      shouldPause = false;
-      return resolve(null);
-    };
-  });
-}
+  function pause() {
+    shouldPause = true;
+    _pause = new Promise((resolve) => {
+      _resolve = () => {
+        shouldPause = false;
+        return resolve(null);
+      };
+    });
+  }
 
-function use_message_bus() {
-  return [_debug_msg, _core_msg, _mem_msg];
-}
+  function use_message_bus() {
+    return [_debug_msg, _core_msg, _mem_msg];
+  }
 
-export async function pause_stub() {
-  if (shouldPause) {
-    dbg(`${_i} is paused`);
+  function lock_critical_region_unlock_cycle(
+    lock_impl,
+    unlock_impl,
+    critical_region
+  ) {
     _running_sync_msg.next({
-      type: "paused",
+      type: "ready",
       payload: _i,
     });
-    await _pause;
+    return async (useMessageBus, pid: number, ...args: any[]) => {
+      _running_sync_msg.next({
+        type: "running",
+        payload: _i,
+      });
+      await lock_impl(useMessageBus, pid, ...args);
+      _core_msg.next(["pre", pid]);
+      await critical_region();
+      _core_msg.next(["post", pid]);
+      await unlock_impl(useMessageBus, pid, ...args);
+      _core_msg.next({ type: "completed", payload: pid });
+    };
   }
-  _running_sync_msg.next({
-    type: "running",
-    payload: _i,
-  });
-  return null;
-}
 
-export async function break_point(lineno: number, message?: string) {
-  _running_sync_msg.next({
-    type: "lineno",
-    payload: { pid: _i, lineno, message },
-  });
-  if (_pauseAtEveryBreakpoint) {
-    pause();
-  }
-  await pause_stub();
-}
-
-function lock_critical_region_unlock_cycle(
-  lock_impl,
-  unlock_impl,
-  critical_region
-) {
-  _running_sync_msg.next({
-    type: "ready",
-    payload: _i,
-  });
-  return async (useMessageBus, pid: number, ...args: any[]) => {
+  async function pause_stub() {
+    if (shouldPause) {
+      dbg(`${_i} is paused`);
+      _running_sync_msg.next({
+        type: "paused",
+        payload: _i,
+      });
+      await _pause;
+    }
     _running_sync_msg.next({
       type: "running",
       payload: _i,
     });
-    await lock_impl(useMessageBus, pid, ...args);
-    _core_msg.next(["pre", pid]);
-    await critical_region();
-    _core_msg.next(["post", pid]);
-    await unlock_impl(useMessageBus, pid, ...args);
-    _core_msg.next({ type: "completed", payload: pid });
-  };
-}
+    return null;
+  }
 
-export const Demo = (lock_impl, unlock_impl, critical_region) => ({
-  memory_msg() {
-    return Observable.from(_mem_msg);
-  },
-  core_msg() {
-    return merge(
-      Observable.from(_core_msg),
-      Observable.from(_running_sync_msg)
-    );
-  },
-  debug_msg() {
-    return Observable.from(_debug_msg);
-  },
-  resume() {
-    if (shouldPause && !isUndefined(_resolve)) {
-      dbg(`resume ${_i}`);
-      return _resolve(null);
-    }
-    dbg(`this cpu is not paused`);
-  },
-  request_pause() {
-    if (!shouldPause) {
-      dbg("request_pause");
+  async function break_point(lineno: number, message?: string) {
+    _running_sync_msg.next({
+      type: "lineno",
+      payload: { pid: _i, lineno, message },
+    });
+    if (_pauseAtEveryBreakpoint) {
       pause();
-    } else {
-      dbg("already paused");
     }
-  },
-  enable_breakpoints() {
-    _pauseAtEveryBreakpoint = true;
-  },
-  async run(pid: number, ...args: any[]) {
-    dbg(`${pid} starts running with args ${JSON.stringify(args)}`);
-    _i = pid;
+    await pause_stub();
+  }
 
-    const ans = await lock_critical_region_unlock_cycle(
-      lock_impl,
-      unlock_impl,
-      critical_region
-    )(use_message_bus, pid, ...args);
+  const Demo = (lock_impl, unlock_impl, critical_region) => ({
+    memory_msg() {
+      return Observable.from(_mem_msg);
+    },
+    core_msg() {
+      return merge(
+        Observable.from(_core_msg),
+        Observable.from(_running_sync_msg)
+      );
+    },
+    debug_msg() {
+      return Observable.from(_debug_msg);
+    },
+    resume() {
+      if (shouldPause && !isUndefined(_resolve)) {
+        dbg(`resume ${_i}`);
+        return _resolve(null);
+      }
+      dbg(`this cpu is not paused`);
+    },
+    request_pause() {
+      if (!shouldPause) {
+        dbg("request_pause");
+        pause();
+      } else {
+        dbg("already paused");
+      }
+    },
+    enable_breakpoints() {
+      _pauseAtEveryBreakpoint = true;
+    },
+    async run(pid: number, ...args: any[]) {
+      dbg(`${pid} starts running with args ${JSON.stringify(args)}`);
+      _i = pid;
 
-    dbg(`${pid} completed`);
-    _pauseAtEveryBreakpoint = false;
-    return [ans, pid];
-  },
-});
+      const ans = await lock_critical_region_unlock_cycle(
+        lock_impl,
+        unlock_impl,
+        critical_region
+      )(use_message_bus, pid, ...args);
+
+      dbg(`${pid} completed`);
+      _pauseAtEveryBreakpoint = false;
+      return [ans, pid];
+    },
+  });
+
+  return { Demo, pause_stub, break_point };
+};
