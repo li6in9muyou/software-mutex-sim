27426ace1bb6f7568ea4f622514306c5227822a1
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-06 16:43:19 +0800

    style: fix typo

--- src/view/InSimulation/Memory.svelte
+++ src/view/InSimulation/Memory.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
   import { get } from "svelte/store";
   import type { MemorySliceStores } from "../../use_case/MemoryWriteSync";
+  import { CurrentSelectedAlgorithm } from "../algorithm_config";
 
   export let stores: MemorySliceStores = null;
 </script>
 
-<h2 class="mb-2 text-2xl underline">shard memory</h2>
+<h2 class="mb-2 text-2xl underline">shared memory</h2>
 {#each Object.keys($stores) as label}
   <div>
     <div class="mb-2 text-xl">{label}</div>
     <div class="mb-3 flex w-full flex-wrap gap-2">
       {#each get($stores[label]) as element}
-        <div class="flex w-fit justify-center border border-secondary text-xl">
-          {element}
+        <div
+          class="flex w-fit justify-center border border-secondary px-1 text-xl"
+        >
+          {$CurrentSelectedAlgorithm.memory_transform(element)}
         </div>
       {/each}
     </div>
