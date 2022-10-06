8804edc9959f6e957f66c37bade5b97af086cb64
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-06 23:13:49 +0800

    test: these tests are not runnable

--- src/test/resources/FakeAlgorithm.ts
+++
@@ -1,6 +0,0 @@
-import { expose } from "threads";
-import { noop } from "lodash";
-import ImportBaseProcessModule from "../../use_case/BaseProcess";
-const { Demo } = ImportBaseProcessModule();
-
-expose(Demo(noop, noop, noop));
