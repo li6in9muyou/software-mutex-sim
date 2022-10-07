5e672af312a10bea8809625165634f721add7e04
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-07 20:34:58 +0800

    wip: buggy

---
+++ src/use_case/WebWorkerProcessAdapter.ts
@@ -0,0 +1,104 @@
+import type { Readable } from "svelte/store";
+import { derived } from "svelte/store";
+import type IProcess from "./IProcess";
+import { ProcessLifeCycle } from "./IProcess";
+import type IProgram from "./IProgram";
+import { LockingState } from "./IProgram";
+import { spawn, Thread } from "threads";
+import { Subject } from "threads/observable";
+import debug from "debug";
+import RunningSync, { ProcessState } from "./RunningSync";
+import ContendingOrNot from "../port/ContendingOrNot";
+import { SveltePort } from "../port/SveltePort";
+
+export default class WebWorkerProcess implements IProcess {
+  private exec_convert(old: ProcessState): ProcessLifeCycle {
+    switch (old) {
+      case ProcessState.completed:
+        return ProcessLifeCycle.completed;
+      case ProcessState.paused:
+        return ProcessLifeCycle.paused;
+      case ProcessState.ready:
+        return ProcessLifeCycle.ready;
+      case ProcessState.running:
+        return ProcessLifeCycle.running;
+    }
+  }
+
+  private locking_convert(
+    acquired: Readable<boolean[]>
+  ): Readable<LockingState> {
+    return derived(acquired, (arr, set) => {
+      const next_state = arr[0] ? LockingState.Locked : LockingState.Unlocked;
+      this.note(`program.locking_state = ${next_state}`);
+      set(next_state);
+    });
+  }
+
+  private toProgram(rs: RunningSync, con: ContendingOrNot): IProgram {
+    const [overview, ,] = con.get_stores_overview_contending_acquired();
+    return {
+      line_number: rs.lineno[0],
+      locking_state: this.locking_convert(overview),
+    };
+  }
+
+  private toExecState(rs: RunningSync): Readable<ProcessLifeCycle> {
+    return derived(rs.running, (arr, set) => {
+      const processLifeCycle = this.exec_convert(arr[0]);
+      this.note(`execution_state = ${processLifeCycle}`);
+      set(processLifeCycle);
+    });
+  }
+
+  execution_state: Readable<ProcessLifeCycle>;
+  program: IProgram;
+  readonly source = new Subject();
+  private impl: any;
+  private readonly note: debug.Debugger;
+
+  async kill(): Promise<void> {
+    await Thread.terminate(this.impl);
+  }
+
+  async pause(): Promise<void> {
+    await this.impl.request_pause();
+  }
+
+  async resume(): Promise<void> {
+    await this.impl.resume();
+  }
+
+  async start(): Promise<void> {
+    await this.impl.run(this.pid, this.shard_memory);
+  }
+
+  constructor(
+    private readonly process_url: URL,
+    public readonly pid: number,
+    private readonly shard_memory
+  ) {
+    this.note = debug(`WebWorkerProcess[${pid}]`);
+    const sub = (msg) => {
+      this.source.next(msg);
+    };
+    (async () => {
+      const c = await spawn(
+        new Worker(this.process_url, {
+          type: "module",
+        })
+      );
+      await c.memory_msg().subscribe(sub);
+      await c.core_msg().subscribe(sub);
+      await c.debug_msg().subscribe(sub);
+      await Thread.events(c).subscribe(sub);
+      return c;
+    })().then((p) => {
+      this.note("spawned.");
+      this.impl = p;
+    });
+    const rs = new RunningSync(1, this.source);
+    this.program = this.toProgram(rs, new ContendingOrNot(new SveltePort(1)));
+    this.execution_state = this.toExecState(rs);
+  }
+}
---
+++ src/use_case/WebWorkerProcessGroupAdapter.ts
@@ -0,0 +1,66 @@
+import type IProcessGroup from "./IProcessGroup";
+import type { IProcessGroupQuery } from "./IProcessGroup";
+import type { Readable, Writable } from "svelte/store";
+import type IProcess from "./IProcess";
+import type { IProcessCommand } from "./IProcess";
+import type { IMemory } from "./MemoryWriteSync";
+import { createMemorySyncStoreAndSync } from "./MemoryWriteSync";
+import WebWorkerProcess from "./WebWorkerProcessAdapter";
+import { get, writable } from "svelte/store";
+import debug from "debug";
+import type { ProcessLifeCycle } from "./IProcess";
+import type IProgram from "./IProgram";
+const note = debug("WebWorkerProcessGroup");
+
+export default class WebWorkerProcessGroup
+  implements IProcessGroup, IProcessGroupQuery, IProcessCommand
+{
+  private readonly processes: IProcess[] = [];
+  memory: Map<string, Writable<Array<number>>> = new Map();
+  execution_state: Readable<ProcessLifeCycle>[];
+  program: IProgram[];
+  async kill(): Promise<void> {
+    await Promise.all(this.processes.map((p) => p.kill()));
+  }
+  async pause(): Promise<void> {
+    await Promise.all(this.processes.map((p) => p.pause()));
+  }
+  async resume(): Promise<void> {
+    await Promise.all(this.processes.map((p) => p.resume()));
+  }
+  async start(): Promise<void> {
+    await Promise.all(this.processes.map((p) => p.start()));
+  }
+  get all(): IProcessGroupQuery & IProcessCommand {
+    return this;
+  }
+  constructor(
+    public readonly process_count: number,
+    private readonly process_url: URL,
+    private readonly get_memory: (process_count: number) => IMemory
+  ) {
+    const memory = this.get_memory(this.process_count);
+    for (const memoryKey in memory) {
+      this.memory.set(memoryKey, writable([]));
+    }
+    const [memory_store, memory_listener] =
+      createMemorySyncStoreAndSync(memory);
+    for (let pid = 0; pid < process_count; pid++) {
+      const process = new WebWorkerProcess(this.process_url, pid, memory);
+      process.source.subscribe(memory_listener);
+      this.processes.push(process);
+    }
+    memory_store.subscribe((stores) => {
+      for (const storeKey in stores) {
+        this.memory.get(storeKey).set(get(stores[storeKey]));
+      }
+    });
+    this.execution_state = this.processes.map((p) => p.execution_state);
+    this.program = this.processes.map((p) => p.program);
+    note(`has ${process_count} processes`);
+  }
+
+  pid(pid: number): IProcess {
+    return this.processes[pid];
+  }
+}
