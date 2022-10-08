bf56778eeb32c11908fcc715473581f02a727fed
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-08 11:18:58 +0800

    fix: memory does not sync

--- src/view/InSimulation/Memory.svelte
+++ src/view/InSimulation/Memory.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-  import { get, type Readable } from "svelte/store";
+  import { derived, type Readable } from "svelte/store";
   import { CurrentSelectedAlgorithm } from "../algorithm_config";
 
   export let stores: Map<string, Readable<Array<number>>> = null;
+  const memKeys = [...stores!.keys()];
+  const memValues = memKeys.map((k) => stores.get(k));
+  const memory = derived(memValues, (ss, set) => {
+    const ans = {};
+    for (let i = 0; i < memValues.length; i++) {
+      ans[memKeys[i]] = ss[i];
+    }
+    set(ans);
+  });
 </script>
 
 <h2 class="mb-2 text-2xl underline">shared memory</h2>
 <section class="flex flex-grow flex-wrap gap-3">
-  {#each [...stores.keys()] as label}
+  {#each memKeys as label}
     <div class="inline">
       <div class="mb-2 text-xl">{label}</div>
       <div class="mb-3 flex w-full flex-wrap gap-2">
-        {#each get(stores.get(label)) as element}
+        {#each $memory[label] as element}
           <div
             class="flex w-fit justify-center border border-secondary px-1 text-xl"
           >
