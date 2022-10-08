b5584e2cdaf7efb00ee8061c95ee6ebfb6dc441b
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-08 12:41:34 +0800

    test: mock execution_state changes

--- src/view/mocks/MockProcessGroup.ts
+++ src/view/mocks/MockProcessGroup.ts
@@ -1,33 +1,37 @@
-import type { Readable } from "svelte/store";
+import type { Readable, Writable } from "svelte/store";
 import type IProcessGroup from "../../use_case/IProcessGroup";
 import type { IProcessGroupQuery } from "../../use_case/IProcessGroup";
 import type IProcess from "../../use_case/IProcess";
 import type { IProcessCommand } from "../../use_case/IProcess";
 import { times } from "lodash";
-import { readable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 import { ProcessLifeCycle } from "../../use_case/IProcessLifeCycle";
 import type IProgram from "../../use_case/IProgram";
 import { LockingState } from "../../use_case/IProgram";
+import debug from "debug";
+import { sleep } from "../../algorithms/utility";
 
 export default class MockProcessGroup implements IProcessGroup {
   readonly memory: Map<string, Readable<Array<number>>>;
   readonly process_count: number = 13;
   readonly all: IProcessGroupQuery & IProcessCommand;
-  private readonly _exec_state: Readable<ProcessLifeCycle>[];
+  private readonly _exec_state: Writable<ProcessLifeCycle>[];
   private readonly _prog: IProgram[];
   private readonly _processes: IProcess[];
+  private readonly note: debug.Debugger;
 
   async noop() {
     return;
   }
 
   constructor() {
+    this.note = debug("MockProcessGroup");
     this._prog = times(this.process_count, () => ({
       locking_state: readable(LockingState.Locking),
       line_number: readable(0),
     }));
     this._exec_state = times(this.process_count, () =>
-      readable(ProcessLifeCycle.ready)
+      writable(ProcessLifeCycle.ready)
     );
     this.all = {
       program: this._prog,
@@ -43,9 +47,9 @@ export default class MockProcessGroup implements IProcessGroup {
     ]);
 
     this._processes = times(this.process_count, (pid) => ({
-      start: this.noop.bind(this),
-      resume: this.noop.bind(this),
-      pause: this.noop.bind(this),
+      start: this.wait_then_set(pid, ProcessLifeCycle.running),
+      resume: this.wait_then_set(pid, ProcessLifeCycle.running),
+      pause: this.wait_then_set(pid, ProcessLifeCycle.paused),
       kill: this.noop.bind(this),
       program: this._prog[pid],
       execution_state: this._exec_state[pid],
@@ -55,5 +59,12 @@ export default class MockProcessGroup implements IProcessGroup {
 
   pid(pid: number): IProcess {
     return this._processes[pid];
+  }
+
+  wait_then_set(pid: number, expected: ProcessLifeCycle, milliseconds = 1000) {
+    return async () =>
+      sleep(milliseconds).then(() => {
+        this._exec_state[pid].set(expected);
+      });
   }
 }
