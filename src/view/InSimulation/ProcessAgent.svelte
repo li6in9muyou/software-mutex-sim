ae106133e87fb7997ed10063652888997ffc9169
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-10-09 17:00:50 +0800

    feat: adjust UI styles

--- src/view/InSimulation/ProcessAgent.svelte
+++ src/view/InSimulation/ProcessAgent.svelte
@@ -5,6 +5,7 @@
   import { LockingState } from "../../use_case/IProgram";
   import { get, type Readable } from "svelte/store";
   import { getContext } from "svelte";
+  import SpinnerAndText from "./SpinnerAndText.svelte";
 
   export let selectedPid: number = null;
   export let ProcessHandle: IProcess = null;
@@ -84,7 +85,7 @@
   class:text-accent-content={isSelected}
   class:bg-base-100={!isSelected}
   class:text-base-content={!isSelected}
-  class="flex h-fit w-fit flex-col items-center gap-3 rounded border border-accent p-2 pt-0 text-xl transition-colors duration-300"
+  class="flex h-fit w-fit flex-col items-start gap-3 rounded border border-accent p-2 pt-0 text-xl transition-colors duration-300"
   on:click={() => (selectedPid = pid)}
 >
   <div>
@@ -96,29 +97,9 @@
   {#if isSelected}
     {#if isIdle}
       <button class="btn btn-success btn-sm ml-auto" on:click={handleStart}>
-        {#if showPauseSpinner}
-          <svg
-            class="-ml-1 mr-3 h-5 w-5 animate-spin text-error"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              class="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            />
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        {/if}
-        start
+        <span class="flex items-center">
+          <SpinnerAndText show={showPauseSpinner} text="start" />
+        </span>
       </button>
     {:else}
       <button
@@ -130,29 +111,9 @@
         class="btn btn-sm ml-auto"
         on:click={handleToggle}
       >
-        {#if showPauseSpinner}
-          <svg
-            class="-ml-1 mr-3 h-5 w-5 animate-spin text-error"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              class="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            />
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        {/if}
-        {buttonFuncLabel}
+        <span class="flex items-center">
+          <SpinnerAndText show={showPauseSpinner} text={buttonFuncLabel} />
+        </span>
       </button>
     {/if}
   {/if}
